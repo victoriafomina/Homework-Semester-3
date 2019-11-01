7697921d2c56afff2341f98f3f638d08ac0a5c5a
Victoria <viky.fomina@yandex.ru>
2019-11-01 14:58:34 +0300

    bad version

---
+++ Chatik/Chatik/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatik
+{
+    public class Client
+    {
+        public Client(int port, IPAddress ip)
+        {
+            this.port = port;
+            this.ip = ip;
+            this.udpClient = new UdpClient();
+            udpClient.Connect(ip, port);
+        }
+
+        public async Task Work()
+        {
+            var sending = Console.ReadLine();
+            if (sending == "exit")
+            {
+                udpClient.Close();
+            }
+            else
+            {
+                Console.WriteLine($"Sending {sending} to port {port}...");
+                var data = Encoding.UTF8.GetBytes("Hi, Server!");
+                await udpClient.SendAsync(data, data.Length, "localhost", port);
+            }
+        }
+
+        private int port;
+        private IPAddress ip;
+        private UdpClient udpClient;
+    }
+}
---
+++ Chatik/Chatik/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+
+namespace Chatik
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution();
+        }
+
+        public static async void Solution()
+        {
+            Console.WriteLine("Input port");
+            var port = Console.ReadLine();
+
+            Console.WriteLine("Input IPAddress. Input '!' if you do not have to input ip");
+            var ip = Console.ReadLine();
+
+            var portNumber = int.Parse(port);
+
+            if (ip != "!")
+            {
+                //var ipAddress = new IPAddress(BitConverter.GetBytes(int.Parse(ip)));
+                //var ip = new IPAddress.
+                var client = new Client(portNumber, IPAddress.Any);
+                await client.Work();
+            }
+            else
+            {
+                var server = new Server(portNumber);
+                await server.Work();
+            }
+        }
+    }
+}
