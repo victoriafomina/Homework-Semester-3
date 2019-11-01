240dcec6dbfa020e077ef44210f4de40cdaf5b4d
Victoria <viky.fomina@yandex.ru>
2019-11-01 13:41:25 +0300

    Created a project

---
+++ SimpleFTP/SimpleFTP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFTP
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const int port = 8888;
+            var udpClient = new UdpClient();
+            Console.WriteLine($"Sending \"Hello!\" to port {port}...");
+            var data = Encoding.UTF8.GetBytes("Hello!");
+            await udpClient.SendAsync(data, data.Length, "localhost", port);
+        }
+    }
+}
