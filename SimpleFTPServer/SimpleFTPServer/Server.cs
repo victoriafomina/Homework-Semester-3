62190a77679f725241533192176fc3df92672d7a
Victoria <viky.fomina@yandex.ru>
2019-11-02 17:15:33 +0300

    i've started implementation of the server

---
+++ SimpleFTPServer/SimpleFTPServer/Server.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+namespace SimpleFTPServer
+{
+    /// <summary>
+    /// Server that handles two requests: List (listing files of the server's directory) and Get (saving
+    /// file from server's directory)
+    /// </summary>
+    public class Server
+    {
+        public Server(int port)
+        {
+
+        }
+
+        private UdpClient udpClient;
+    }
+}
