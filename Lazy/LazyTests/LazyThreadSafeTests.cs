6ad2d21d1d1218001239f470b892aa785790292f
Victoria <viky.fomina@yandex.ru>
2019-12-06 13:38:49 +0300

    Changes

--- Lazy/LazyTests/LazyThreadSafeTests.cs
+++ Lazy/LazyTests/LazyThreadSafeTests.cs
@@ -14,16 +14,28 @@ namespace LazyTests
             Func<int> thirdPowerOf5 = () => 5 * 5 * 5;
             var lazy = LazyFactory<int>.CreateThreadSafe(thirdPowerOf5);
             var numberOfThreads = 100;
+            ManualResetEvent sync = new ManualResetEvent(false);
 
             var threads = new Thread[numberOfThreads];
+            var results = new int[numberOfThreads];
 
             for (var i = 0; i < numberOfThreads; ++i)
             {
-                threads[i] = new Thread(() =>
-                {
-                    Assert.AreEqual(1, lazy.Get());
+                threads[i] = new Thread(() => {
+                    results[i] = lazy.Get();
                 });
+
+                sync.Set();
             }
+
+            sync.WaitOne();
+
+            for (var i = 0; i < numberOfThreads; ++i)
+            {
+                threads[i].Start();
+            }
+
+            for
         }
     }
 }
