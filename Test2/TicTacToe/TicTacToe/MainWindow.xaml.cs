45d63b5ad7b603ad19c3698639957b572ef8eae5
Victoria <viky.fomina@yandex.ru>
2019-12-06 15:17:22 +0300

    Finished

--- Test2/TicTacToe/TicTacToe/MainWindow.xaml.cs
+++ Test2/TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -8,126 +8,57 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
-        private char player = 'x';
+        TicTacToeLogic ticTacToe;
+        private bool gameEnded;
 
+        /// <summary>
+        /// Initializes an instance of the MainWindow.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            ticTacToe = new TicTacToeLogic(button00, button01, button02, button10, button11, button12, button20, button21, button22);
+            gameEnded = false;
         }
 
+        /// <summary>
+        /// Handles field's button click.
+        /// </summary>
         private void ButtonField_Click(object sender, RoutedEventArgs e)
         {
-            MakeAMove(sender as Button);
+            if (!gameEnded)
+            {
+                ticTacToe.MakeAMove(sender as Button, button00, button01, button02, button10, button11, button12, button20, button21, button22);
+                if (ticTacToe.SomeoneWon(button00, button01, button02, button10, button11, button12, button20, button21, button22) ||
+                        ticTacToe.NoWayToMove(button00, button01, button02, button10, button11, button12, button20, button21, button22))
+                {
+                    gameEnded = true;
+                }
+
+                if (gameEnded)
+                {
+                    if (ticTacToe.SomeoneWon(button00, button01, button02, button10, button11, button12, button20, button21, button22))
+                    {
+                        MessageBox.Show($"Player {ticTacToe.GetPlayer().ToString().ToUpper()} won!");
+                    }
+                    else if (ticTacToe.NoWayToMove(button00, button01, button02, button10, button11, button12, button20, button21, button22))
+                    {
+                        MessageBox.Show("Draw!");
+                    }
+
+                    playAgain.Visibility = Visibility.Visible;
+                }
+            }            
         }
 
+        /// <summary>
+        /// Handles play again button click.
+        /// </summary>
         private void ButtonPlayAgain_Click(object sender, RoutedEventArgs e)
         {
-            player = 'x';
-            button00.Content = "";
-            button01.Content = "";
-            button02.Content = "";
-            button10.Content = "";
-            button11.Content = "";
-            button12.Content = "";
-            button20.Content = "";
-            button21.Content = "";
-            button22.Content = "";
-        }
-
-        public void MakeAMove(Button buttonWantToMove)
-        {
-            if (buttonWantToMove.Content.ToString() == "")
-            {
-                switch (player)
-                {
-                    case 'o':
-                        buttonWantToMove.Content = "o";
-                        break;
-                    case 'x':
-                        buttonWantToMove.Content = "x";
-                        break;
-                }
-
-                buttonWantToMove.FontSize = 80;
-
-                if (SomeoneWon())
-                {
-                    MessageBox.Show($"Player {GetPlayer().ToString().ToUpper()} won!");
-                }
-                else if (NoWayToMove())
-                {
-                    MessageBox.Show("Draw!");
-                }
-
-                if (!SomeoneWon() && !NoWayToMove())
-                {
-                    ChangePlayer();
-                }
-            }
-        }
-
-        private char GetPlayer() => player;
-
-        private void ChangePlayer()
-        {
-            if (player == 'o')
-            {
-                player = 'x';
-            }
-            else if (player == 'x')
-            {
-                player = 'o';
-            }
-        }
-
-        private bool SomeoneWon()
-        {
-            if (button00.Content.ToString() != "" && button00.Content == button01.Content && button01.Content == button02.Content)
-            {
-                return true;
-            }
-            else if (button10.Content.ToString() != "" && button10.Content == button11.Content && button11.Content == button12.Content)
-            {
-                return true;
-            }
-            else if (button20.Content.ToString() != "" && button20.Content == button21.Content && button21.Content == button22.Content)
-            {
-                return true;
-            }
-            else if (button00.Content.ToString() != "" && button00.Content == button10.Content && button10.Content == button20.Content)
-            {
-                return true;
-            }
-            else if (button01.Content.ToString() != "" && button01.Content == button11.Content && button11.Content == button21.Content)
-            {
-                return true;
-            }
-            else if (button02.Content.ToString() != "" && button02.Content == button12.Content && button12.Content == button22.Content)
-            {
-                return true;
-            }
-            else if (button00.Content.ToString() != "" && button00.Content == button11.Content && button11.Content == button22.Content)
-            {
-                return true;
-            }
-            else if (button20.Content.ToString() != "" && button20.Content == button11.Content && button11.Content == button02.Content)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        private bool NoWayToMove()
-        {
-            if (button00.Content.ToString() != "" && button01.Content.ToString() != "" && button02.Content.ToString() != "" && button10.Content.ToString() != "" &&
-                    button11.Content.ToString() != "" && button12.Content.ToString() != "" && button20.Content.ToString() != "" &&
-                    button21.Content.ToString() != "" && button22.Content.ToString() != "")
-            {
-                return true;
-            }
-
-            return false;
+            ticTacToe.PlayAgain(button00, button01, button02, button10, button11, button12, button20, button21, button22);
+            playAgain.Visibility = Visibility.Collapsed;
+            gameEnded = false;
         }
     }
 }
